2df862d5b5741419998c73e4f655fdd3d81048a4
Jajav33 <to.jajav33@gmail.com>
2021-09-19 00:40:33 -0700

    adding quasar and a canvas

---
+++ packages/app/src/components/TileGenCanvas.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="fit">
+    <canvas class="fit" style="border: 2px solid black" :height="`${$props.tile_height * $props.rows}`" :width="`${$props.tile_width * $props.columns}`"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  PropType,
+  computed,
+  ref,
+  toRef,
+  Ref,
+} from 'vue';
+import { Meta } from './models';
+
+export default defineComponent({
+  name: 'TileGenCanvas',
+  props: {
+    tile_width: {
+      type: Number,
+      required: true
+    },
+    tile_height: {
+      type: Number,
+      required: true
+    },
+    rows: {
+      type: Number,
+      required: true
+    },
+    columns: {
+      type: Number,
+      required: true
+    },
+    meta: {
+      type: Object as PropType<Meta>,
+      required: false
+    },
+  },
+  setup(props) {
+    return { };
+  },
+});
+</script>
---
+++ packages/app/src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-form class="row">
+      <q-input outlined v-model="settings.tile_width" label="Tile Width" ></q-input>
+      <q-input outlined v-model="settings.tile_height" label="Tile Height" ></q-input>
+      <q-input outlined v-model="settings.rows" label="Rows" ></q-input>
+      <q-input outlined v-model="settings.columns" label="Columns" ></q-input>
+    </q-form>
+    <tile-gen-canvas
+      :tile_height="settings.tile_height"
+      :tile_width="settings.tile_width"
+      :rows="settings.rows"
+      :columns="settings.columns"
+    ></tile-gen-canvas>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import TileGenCanvas from '../components/TileGenCanvas.vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: { TileGenCanvas },
+  setup() {
+    const settings = {
+      tile_width: 64,
+      tile_height: 64,
+      rows: 4,
+      columns: 12
+    };
+
+    return {
+      settings
+    }
+  }
+});
+</script>
